43f52054c10a5ca689dd8ecddf6151c5ed3fc96a
Paulo Conter <conterdev@gmail.com>
2023-11-06 15:38:22 -0300

    Add files via upload

---
+++ Formulario.aspx.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Projeto_Painel : System.Web.UI.Page
+    {
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+
+
+            string sexoSelecionado = ddlSexo.SelectedValue;
+            string nome = txtNome.Text;
+            string sobrenome = txtSobrenome.Text;
+            string celular = TxtCelular.Text;
+
+
+            if (string.IsNullOrWhiteSpace(sexoSelecionado) || string.IsNullOrWhiteSpace(nome) || string.IsNullOrWhiteSpace(sobrenome) || string.IsNullOrWhiteSpace(celular))
+
+            {
+                lblMensagemErro.Text = "Preencha todos os campos obrigatórios.";
+                lblMensagemErro.Visible = true;
+            }
+            else
+            {
+
+                PnlInfPessoais.Visible = false;
+                PnlEndPessoal.Visible = true;
+                PnlLogin.Visible = false;
+
+            }
+
+        }
+
+        protected void BtnVoltar_Click(object sender, EventArgs e)
+        {
+            PnlInfPessoais.Visible = true;
+            PnlEndPessoal.Visible = false;
+            PnlLogin.Visible = false;
+
+            lblMensagemErro.Text = string.Empty;
+            lblMensagemErro.Visible = false;
+        }
+
+        protected void BtnProxEnd_Click(object sender, EventArgs e)
+        {
+            string Endereco = TextEnd.Text;
+            string Cidade = TextCid.Text;
+            string Cep = TextCep.Text;
+
+            if (string.IsNullOrWhiteSpace(Endereco) || string.IsNullOrWhiteSpace(Cidade) || string.IsNullOrWhiteSpace(Cep))
+            {
+
+                lblMensagemErro1.Text = "Preencha todos os campos obrigatórios.";
+                lblMensagemErro1.Visible = true;
+            }
+
+            else
+            {
+                PnlInfPessoais.Visible = false;
+                PnlEndPessoal.Visible = false;
+                PnlLogin.Visible = true;
+            }
+        }
+
+        protected void BtnVoltar2_Click(object sender, EventArgs e)
+        {
+            PnlInfPessoais.Visible = false;
+            PnlEndPessoal.Visible = true;
+            PnlLogin.Visible = false;
+
+            lblMensagemErro1.Text = string.Empty;
+            lblMensagemErro1.Visible = false;
+        }
+
+        protected void BtnEnv_Click(object sender, EventArgs e)
+        {
+            string Usuario = TextUsuario.Text;
+            string Senha = TextSenha.Text;
+
+            if (string.IsNullOrWhiteSpace(Usuario) || string.IsNullOrWhiteSpace(Senha))
+            {
+                lblMensagemErro2.Text = "Por favor, preencha todos os campos.";
+                lblMensagemErro2.Visible = true;
+            }
+
+            else
+            {
+                lblMensagemSucesso.Text = "Dados enviados com sucesso!";
+                lblMensagemSucesso.Visible = true;
+
+                //Aqui eu chamo um Alert na tela informando que deu tudo certo e os dados foram enviados usando uma função em Javascript
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('" + lblMensagemSucesso.Text + "');", true);
+            }
+        }
+        
+    }
+}
---
+++ Formulario.aspx.designer.cs
@@ -0,0 +1,224 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace WebApplication1
+{
+
+
+	public partial class Projeto_Painel
+	{
+
+		/// <summary>
+		/// Controle form1.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+		/// <summary>
+		/// Controle PnlPrincipal.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Panel PnlPrincipal;
+
+		/// <summary>
+		/// Controle PnlInfPessoais.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Panel PnlInfPessoais;
+
+		/// <summary>
+		/// Controle txtNome.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox txtNome;
+
+		/// <summary>
+		/// Controle txtSobrenome.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox txtSobrenome;
+
+		/// <summary>
+		/// Controle ddlSexo.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.DropDownList ddlSexo;
+
+		/// <summary>
+		/// Controle TxtCelular.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox TxtCelular;
+
+		/// <summary>
+		/// Controle lblMensagemErro.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Label lblMensagemErro;
+
+		/// <summary>
+		/// Controle btnSubmit.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Button btnSubmit;
+
+		/// <summary>
+		/// Controle PnlEndPessoal.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Panel PnlEndPessoal;
+
+		/// <summary>
+		/// Controle TextEnd.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox TextEnd;
+
+		/// <summary>
+		/// Controle TextCid.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox TextCid;
+
+		/// <summary>
+		/// Controle TextCep.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox TextCep;
+
+		/// <summary>
+		/// Controle lblMensagemErro1.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Label lblMensagemErro1;
+
+		/// <summary>
+		/// Controle BtnVoltar.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Button BtnVoltar;
+
+		/// <summary>
+		/// Controle BtnProxEnd.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Button BtnProxEnd;
+
+		/// <summary>
+		/// Controle PnlLogin.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Panel PnlLogin;
+
+		/// <summary>
+		/// Controle TextUsuario.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox TextUsuario;
+
+		/// <summary>
+		/// Controle TextSenha.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox TextSenha;
+
+		/// <summary>
+		/// Controle lblMensagemErro2.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Label lblMensagemErro2;
+
+		/// <summary>
+		/// Controle BtnVoltar2.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Button BtnVoltar2;
+
+		/// <summary>
+		/// Controle BtnEnv.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Button BtnEnv;
+
+		/// <summary>
+		/// Controle lblMensagemSucesso.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Label lblMensagemSucesso;
+	}
+}
